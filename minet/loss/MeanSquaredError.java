f0d03325b670a38f05cd4814f9cd6a1bce19845f
ndangtt <ndangt@gmail.com>
2020-10-01 22:33:10 +0100

    update MSE for classification

--- minet/loss/MeanSquaredError.java
+++ minet/loss/MeanSquaredError.java
@@ -17,14 +17,20 @@ public class MeanSquaredError implements Loss {
     public MeanSquaredError() { }
 
     /**
-     * Compute a loss value given groud-truth Y and estimate Yhat
+     * Compute a loss value given ground-truth Y and estimate Yhat
      * @param Y a [minibatch_size x d] matrix, each row is the ground-truth of an instance
      * @param Yhat a [minibatch_size x d] matrix, each row is an estimate of an instance
      * @return a double
      */
     @Override
     public double forward(DoubleMatrix Y, DoubleMatrix Yhat) {
-        this.Y = Y.dup();
+    	// if being used for classification, the ground-truth is a vector, so we need to make it become a matrix    	
+    	if ((Y.columns==1) && Yhat.columns>1) {    		
+    		this.Y = DoubleMatrix.zeros(Yhat.rows, Yhat.columns);    		
+            for (int i = 0; i < Y.rows; i++) {
+                this.Y.put(i, (int)Y.get(i,0), 1);
+            }
+    	}      
         this.Yhat = Yhat.dup();
         return MatrixFunctions.powi(Y.sub(Yhat), 2.).columnSums().sum() / Y.rows;
     }
