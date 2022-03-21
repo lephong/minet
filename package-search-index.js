a624cd98f1bd0143818b3eabe39e160cd0ffe962
ndangtt <ndangtt@gmail.com>
2022-03-21 22:54:44 +0000

    add java doc

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"minet.data"},{"l":"minet.example.mnist"},{"l":"minet.layer"},{"l":"minet.layer.init"},{"l":"minet.loss"},{"l":"minet.optim"},{"l":"minet.util"}];updateSearchResults();
